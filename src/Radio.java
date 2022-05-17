764090119d740f5553f982626b7026ff5f1180e5
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-17 10:09:44 -0400

    Radio class

---
+++ src/Radio.java
@@ -0,0 +1,15 @@
+public class Radio {
+    String station;
+
+    public Radio() {
+        station = "1060 AM";
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String s) {
+        this.station = s;
+    }
+}
