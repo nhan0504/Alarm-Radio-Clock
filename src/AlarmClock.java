3cabbfc90d5adb9469d3e850872185fa6e834210
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-17 09:58:55 -0400

    Alarm clock that maintain alarm time

---
+++ src/AlarmClock.java
@@ -0,0 +1,44 @@
+public class AlarmClock extends Clock {
+    Time alarmTime;
+    boolean turnOn;
+
+    public AlarmClock() {
+        alarmTime = new Time(8,5);
+        turnOn = false;
+    }
+
+    public void setAlarmTime(Time t) {
+        alarmTime.hour = t.hour;
+        alarmTime.min = t.min;
+    }
+
+    public void turnOn() {
+        turnOn = true;
+    }
+
+    public void turnOff() {
+        turnOn = false;
+    }
+
+    public Time getAlarmTime() {
+        return alarmTime;
+    }
+
+    public boolean isAlarmOn() {
+        return turnOn;
+    }
+
+    private void alarmSound() {
+        System.out.println("buzzlightyear");
+    }
+
+    public void snooze(int min) {
+        for (int i = 0; i < min; i++) {
+            time.tick();
+            if (time.equals(alarmTime)) {
+                alarmSound();
+            }
+        }
+
+    }
+}
