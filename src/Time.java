bcf3f5856d8cb7d85ea8abffe9fda52923bf352d
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-18 09:16:03 -0400

    Add sec field

--- src/Time.java
+++ src/Time.java
@@ -1,15 +1,18 @@
 public class Time {
     int hour;
     int min;
+    int sec;
 
     public Time() {
         this.hour = 8;
         this.min = 0;
+        this.sec = 0;
     }
 
-    public Time(int hour, int min) {
+    public Time(int hour, int min, int sec) {
         this.hour = hour;
         this.min = min;
+        this.sec = sec;
     }
 
     public void showTime() {
@@ -20,17 +23,14 @@ public class Time {
         }
     }
 
-    public int getHour(Time t) {
-        return t.hour;
-    }
-
     public void setTime(Time t) {
         this.hour = t.hour;
         this.min = t.min;
+        this.sec = t.sec;
     }
 
     public boolean equals(Time t) {
-        if (t.hour == this.hour && t.min == this.min) {
+        if (t.hour == this.hour && t.min == this.min && t.sec == this.sec) {
             return true;
         }
         return false;
@@ -38,11 +38,16 @@ public class Time {
 
     public void tick() {
         for(int i = 0; i < 60; i++) {
-            if (min < 59) {
-                this.min++;
+            if (sec < 59) {
+                this.sec++;
             } else {
-                this.hour++;
-                this.min = 0;
+                this.sec = 0;
+                if (min < 59) {
+                    this.min++;
+                } else {
+                    this.min = 0;
+                    this.hour++;
+                }
             }
         }
     }
