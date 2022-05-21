714ccca6046b109a549898fc3d2623dca7f3f298
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-21 09:18:15 -0400

    Manual test

--- src/AlarmRadioClockMain.java
+++ src/AlarmRadioClockMain.java
@@ -20,5 +20,22 @@ public class AlarmRadioClockMain {
         alarmClock.getAlarmTime().showTime();
         alarmClock.turnOff();
         System.out.println("Alarm was shut off");
+
+        AlarmRadioClock alarmRadioClock = new AlarmRadioClock(8,0,0,"AM","1060AM");
+        alarmRadioClock.setAlarm(8, 5, 0, "AM");
+        alarmRadioClock.turnOn();
+        for (int i = 0; i < 5; i++) {
+            alarmRadioClock.tick();
+            alarmRadioClock.showTime();
+            if (alarmRadioClock.isAlarmOn()) {
+                if (alarmRadioClock.checkAlarm()) {
+                    alarmRadioClock.snooze();
+                }
+            }
+        }
+        alarmRadioClock.snooze();
+        alarmRadioClock.getAlarmTime().showTime();
+        alarmRadioClock.turnOff();
+        System.out.println("Alarm was shut off");
     }
 }
